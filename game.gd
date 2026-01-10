2daf5fa589034b4f53c4afd217fbf8b0202a014a
that-damn-cat <saihenjin@gmail.com>
2026-01-10 11:11:56 -0600

    game generates trips, event scene queries current event from game

--- game.gd
+++ game.gd
@@ -1,5 +1,8 @@
 class_name Game
-extends Control
+extends Node2D
+
+@export var screen_shaker: ScreenShaker
+@export var control_root: Control
 
 var ship: Ship
 
@@ -7,12 +10,45 @@ var break_events: Array[Event] = []
 var resource_events: Array[Event] = []
 var route_events: Array[Event] = []
 
+var trip: Array[Event]
+var trip_step: int = 0
+
 func _ready() -> void:
 	ship = Ship.new_ship()
 
 	break_events = _array_from_folder(Constants.BREAK_EVENT_FOLDER)
 	resource_events = _array_from_folder(Constants.RESOURCE_EVENT_FOLDER)
 	route_events = _array_from_folder(Constants.ROUTE_EVENT_FOLDER)
+
+	_on_viewport_resize()
+	get_viewport().size_changed.connect(_on_viewport_resize)
+
+	generate_trip()
+
+
+func generate_trip() -> void:
+	for i in range(Constants.INITIAL_TRIP_LENGTH):
+		add_stop(break_events)
+
+func add_event_stop(count: int) -> void:
+	for i in range(count):
+		add_stop([break_events, resource_events].pick_random())
+
+func remove_event_stop(count: int) -> void:
+	for i in range(count):
+		trip.remove_at(randi_range(trip_step, trip.size()))
+
+func add_stop(event_array: Array[Event]) -> void:
+	trip.append(event_array.pick_random())
+
+func get_current_event() -> Event:
+	return(trip[trip_step])
+
+func advance_trip() -> void:
+	trip_step += 1
+	if trip_step >= trip.size():
+		print("OUT OF STEPS!")
+		trip_step = 0
 
 func _array_from_folder(folder: String) -> Array[Event]:
 	var array: Array[Event] = []
@@ -25,3 +61,7 @@ func _array_from_folder(folder: String) -> Array[Event]:
 		array.append(ResourceLoader.load(filepath))
 
 	return(array)
+
+func _on_viewport_resize() -> void:
+	position = get_viewport_rect().size / 2
+	control_root.custom_minimum_size = get_viewport_rect().size
